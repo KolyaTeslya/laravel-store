0d295cb5e7a55357a504944a69df83b6e61458ec
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-07-31 16:46:41 +0300

    add basket for product

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
 
 Route::get('/', [\App\Http\Controllers\PageController::class, 'index'])->name('index');
 
@@ -21,7 +22,16 @@ Route::resource('catalog', \App\Http\Controllers\CatalogController::class)->para
     'catalog' => 'slug'
 ]);
 
-Auth::routes();
+Route::prefix('cart')->group(function (){
+    Route::get('/', [\App\Http\Controllers\CartController::class, 'index'])->name('cart.index');
+    Route::get('add/{productId}', [\App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
+    Route::patch('update', [\App\Http\Controllers\CartController::class, 'update'])->name('cart.update');
+    Route::get('drop', [\App\Http\Controllers\CartController::class, 'drop'])->name('cart.drop');
+    Route::get('destroy', [\App\Http\Controllers\CartController::class, 'destroy'])->name('cart.destroy');
+    Route::get('checkout', [\App\Http\Controllers\CartController::class, 'checkout'])->name('cart.checkout');
+
+});
+
 
 
 
